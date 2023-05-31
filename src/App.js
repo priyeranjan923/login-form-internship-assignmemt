d215c490a4b10bc60c2a7a0f005d3977cf3fc028
priyeranjan923 <priyeranjan923@gmail.com>
2023-06-01 02:22:12 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Loginpage from "./components/Loginpage"
+function App() {
+  return (
+   <div className="w-screen min-h-screen">
+    <Loginpage/>
+   </div>
+  );
+}
+
+export default App;
